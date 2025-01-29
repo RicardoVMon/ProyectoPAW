a33a39ce7eb2fc46789620c31b6fb409bd0577c2
LuisDavid01 <luisdavidmirandav12@gmail.com>
2025-01-28 18:42:54 -0600

    Prueba login con identity api

---
+++ Práctica 1 - Grupo #8/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Práctica_1___Grupo__8
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- Práctica 1 - Grupo #8/Program.cs
+++ Práctica 1 - Grupo #8/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Práctica_1___Grupo__8;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+//se crea la conexion con la base de datos
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer("name=DefaultConnection"));
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
+//Implementamos el api de identity framework para realizar los registros , logins
+
+app.MapGroup("/identity").MapIdentityApi<IdentityUser>();
+builder.Services.AddAuthorization();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
